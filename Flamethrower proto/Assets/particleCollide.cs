eac4ace5fba41a3a9c5b8acc2959e202c24a7297
jjam02 <35983461+jjam02@users.noreply.github.com>
2023-12-10 16:09:49 -0800

    remove ver control

--- Flamethrower proto/Assets/particleCollide.cs
+++ Flamethrower proto/Assets/particleCollide.cs
@@ -7,7 +7,7 @@ public class particleCollide : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Debug.Log("the console is working");
     }
 
     // Update is called once per frame
