d87721b9f56b14885905dd52dedfc377e6c054cd
jjam02 <35983461+jjam02@users.noreply.github.com>
2023-12-10 21:48:58 -0800

    basic enemy tracks player

--- Flamethrower proto/Assets/reset.cs
+++ Flamethrower proto/Assets/reset.cs
@@ -6,6 +6,8 @@ public class reset : MonoBehaviour
 {
     // Start is called before the first frame update
     private Vector3 initialPos;
+    public float speed;
+    public GameObject player;
 
     void Start()
     {
@@ -15,7 +17,13 @@ public class reset : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        speed = Random.Range(1, 4);
+
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.T))
         {
             transform.position = initialPos;
         }
