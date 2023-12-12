1fc904daf6362cf2792094e164ce60b0723a1a24
jjam02 <35983461+jjam02@users.noreply.github.com>
2023-12-11 16:34:59 -0800

    for fun changes

--- Flamethrower proto/Assets/controller.cs
+++ Flamethrower proto/Assets/controller.cs
@@ -7,15 +7,31 @@ public class controller : MonoBehaviour
     public float moveSpeed = 5f;
     public float forceMultiplier = 10f;
 
+    [SerializeField]
+    private GameObject[] enemies;
+
     private Vector3 initialPos;
+
+    private Camera mainCamera;
+    private float playerHeight;
+    private float playerWidth;
+    // Define the bounds
+    float minX = -8.34f;
+    float maxX = 8.26f;
+    float minY = -3.44f;
+    float maxY = 3.57f;
+
+    public int enemiesHit = 0;
 
     void Start()
     {
+        mainCamera = Camera.main;
         initialPos = transform.position;
     }
 
     void Update()
     {
+
         // Check for mouse click
         if (Input.GetMouseButton(0)) // 0 corresponds to the left mouse button
         {
@@ -32,6 +48,33 @@ public class controller : MonoBehaviour
             GetComponent<Rigidbody2D>().AddForce(-direction * forceMultiplier);
         }
 
+        // Check if the player is below the camera's bottom edge
+        if (transform.position.y < mainCamera.transform.position.y - mainCamera.orthographicSize - playerHeight / 2)
+        {
+            // Reposition the player at the top of the camera
+            RepositionPlayerAtTop();
+        }
+
+        if (transform.position.x > mainCamera.transform.position.x + mainCamera.orthographicSize * mainCamera.aspect + playerWidth / 2)
+        {
+            // Reposition the player on the left side of the camera
+            RepositionPlayerAtLeft();
+        }
+        // Check if the player is above the camera's top edge
+        if (transform.position.y > mainCamera.transform.position.y + mainCamera.orthographicSize + playerHeight / 2)
+        {
+            // Reposition the player at the bottom of the camera
+            RepositionPlayerAtBottom();
+        }
+
+        //Check if the player is beyond the camera's left edge
+        if (transform.position.x < mainCamera.transform.position.x - mainCamera.orthographicSize * mainCamera.aspect - playerWidth / 2)
+        {
+            // Reposition the player on the right side of the camera
+            RepositionPlayerAtLeft();
+            Debug.Log("LEFT WALL");
+        }
+
         // Move the player based on input (optional)
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -43,5 +86,58 @@ public class controller : MonoBehaviour
             transform.position = initialPos;
         }
     }
+
+    void RepositionPlayerAtTop()
+    {
+        // Calculate the new position at the top of the camera
+        float newY = mainCamera.transform.position.y + mainCamera.orthographicSize + playerHeight / 2;
+
+        // Reposition the player
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        repositionEnemies();
+    }
+
+    void RepositionPlayerAtLeft()
+    {
+        // Calculate the new position on the left side of the camera
+        float newX = mainCamera.transform.position.x - mainCamera.orthographicSize * mainCamera.aspect - playerWidth / 2;
+
+        // Reposition the player
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+
+
+        repositionEnemies();
+    }
+
+    void RepositionPlayerAtBottom()
+    {
+        // Calculate the new position at the bottom of the camera
+        float newY = mainCamera.transform.position.y - mainCamera.orthographicSize - playerHeight / 2;
+
+        // Reposition the player
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        repositionEnemies();
+    }
+
+    void RepositionPlayerAtRight()
+    {
+        // Calculate the new position on the right side of the camera
+        float newX = mainCamera.transform.position.x + mainCamera.orthographicSize * mainCamera.aspect + playerWidth / 2;
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        repositionEnemies();
+    }
+
+
+    void repositionEnemies()
+    {
+        foreach (GameObject enemy in enemies)
+
+        {
+            float randomX = Random.Range(minX, maxX);
+            float randomY = Random.Range(minY, maxY);
+            enemy.transform.position = new Vector3(randomX, randomY, 0f);
+        }
+    }
 }
+
 
--- Flamethrower proto/Assets/flameCollider.cs
+++ Flamethrower proto/Assets/flameCollider.cs
@@ -11,9 +11,10 @@ public class flameCollider : MonoBehaviour
     float maxX = 8.26f;
     float minY = -3.44f;
     float maxY = 3.57f;
+    controller playerController;
     void Start()
     {
-
+        playerController = transform.root.GetComponentInChildren<controller>();
     }
 
     // Update is called once per frame
@@ -35,8 +36,72 @@ public class flameCollider : MonoBehaviour
                 float randomY = Random.Range(minY, maxY);
 
                 collision.transform.position = new Vector3(randomX, randomY, 0f);
+
                 Health = 1f;
+                playerController.enemiesHit++;
             }
         }
     }
 }
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class flameCollider : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     float Health = 1f;
+//     // Define the bounds
+//     float minX = -8.34f;
+//     float maxX = 8.26f;
+//     float minY = -3.44f;
+//     float maxY = 3.57f;
+
+//     public GameObject player;
+//     controller playerController;
+//     void Start()
+//     {
+//         controller playerController = GetComponentInParent<controller>();
+//         if (playerController != null)
+//         {
+//             // Access the enemiesHit variable from the PlayerController script
+//             int hitCount = playerController.enemiesHit;
+
+//             // Now you can use hitCount as needed
+//             Debug.Log("Enemies hit: " + hitCount);
+//         }
+//         else
+//         {
+//             Debug.LogError("PlayerController script not found in ancestors of this GameObject.");
+//         }
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+
+//     }
+
+//     void OnCollisionEnter2D(Collision2D collision)
+//     {
+//         if (collision.gameObject.CompareTag("enemy"))
+//         {
+
+//             Health -= 1f;
+//             Debug.Log(Health);
+//             if (Health <= 0)
+//             {
+//                 playerController.enemiesHit++;
+
+//                 float randomX = Random.Range(minX, maxX);
+//                 float randomY = Random.Range(minY, maxY);
+
+//                 collision.transform.position = new Vector3(randomX, randomY, 0f);
+//                 Health = 1f;
+//             }
+//         }
+//     }
+// }
+
